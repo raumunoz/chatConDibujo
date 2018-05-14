88efbb7921053fd775e3c6e5ec63ca881b71194f
rau <c.sa.r@hotmail.com>
2018-05-13 23:15:56 -0500

    comit para mandar a heroku

--- public/sketch.js
+++ public/sketch.js
@@ -30,7 +30,7 @@ myCanvas.mouseOut(()=>canvasPrecionado=false);
 //myCanvas.mouseMoved(dibuja);
 background(51);
 
-socket=io.connect('http://localhost:3000');
+socket=io.connect('http://myapp.herokuapp.com/');
 socket.on('mouse', newDrw);
 socket.on('mensaje chat',agregarMensaje);
 socket.on('usuario local',definirUsuarioLocal);
